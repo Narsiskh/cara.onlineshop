c5d8545408fef986194bc2adb807a86f0fe9c1c6
Narsis <narsiskh.2013@gmail.com>
2023-12-10 12:52:51 +0330

    "Refactored project with TypeScript and removed Font Awesome"

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import "@fortawesome/fontawesome-free/js/all"
 
 
 const app = createApp(App)
