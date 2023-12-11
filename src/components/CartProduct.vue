3e2bc259118f46aa0add5d3e5dd2f0b8f0e8efcd
Narsis <narsiskh.2013@gmail.com>
2023-12-11 19:47:58 +0330

    Create CartProduct component.

---
+++ src/components/CartProduct.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type {Product} from "@/interfaces"
+
+defineProps<{
+  product: Product,
+  total: number
+}>()
+</script>
+
+<template>
+  <tr>
+    <td><a href="#"><i class="bi bi-x-circle"></i></a></td>
+    <td><img src="../img/products/f2.jpg"></td>
+    <td>{{ product.name }}</td>
+    <td>{{ product.price }}</td>
+    <td><input type="number" value="1"></td>
+    <td>${{ total }}</td>
+  </tr>
+</template>
+
+<style scoped lang="sass">
+
+</style>
